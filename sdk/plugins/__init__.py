ba857c3ed17df0346212333d2d712c812f12f6db
Ivan Leon <ivan@levelops.io>
2020-04-12 15:15:03 -0700

    CHG: sdk plugin runner support for multi products and labels as arguments

--- sdk/plugins/__init__.py
+++ sdk/plugins/__init__.py
@@ -5,9 +5,16 @@ from .plugins import Plugin
 from .runner import Runner
 
 
-def labels_parser(labels_str):
-    labels = ""
-    return labels
+def labels_parser(labels_str: str):
+  labels = {}
+  if labels_str and len(labels_str) > 0:
+    tmp = labels_str.split(',')
+    for l in tmp:
+      label = l.split('=')
+      values = labels.get(label[0], [])
+      values.append(label[1])
+      labels[label[0]] = values
+  return labels
 
 
 default_plugin_options = [
@@ -18,7 +25,7 @@ default_plugin_options = [
   {'args':['--out'], 'kwords':{'dest': 'output_file', 'help': 'Path to the destination file/directory. If present and valid, the scanner results will be written to the output file/directory. If the path is a directory, the result(s) will be written inside it with the prefix "levelops-"'}},
   {'args':['--apikey'], 'kwords':{'dest': 'token', 'help': 'If present, the scanner results will be submited to levelops and if the apiKey is correct, the results will be available to be reviewed and acted on at the customer\'s levelops instance.'}},
   {'args':['--submit'], 'kwords':{'dest': 'submit', 'help': 'If present, the results will be submited to levelops.', 'action': 'store_true'}},
-  {'args':['--labels'], 'kwords':{'dest': 'labels', 'help': 'If submit is present, the coma separated list of labels will be part of the results sent to levelops.', 'type':labels_parser}},
+  {'args':['--labels'], 'kwords':{'dest': 'labels', 'help': 'If submit is present, the coma separated list of labels (label_name=label_value) will be part of the results sent to levelops. Multiple values for the same label are supported by adding more than one label with the same label name (label1=value1,label1=value2).', 'type':labels_parser}},
   {'args':['--endpoint'], 'kwords':{'dest': 'endpoint', 'help': 'If submit is present, the hostname and protocol can be customize with this flag (default: https://api.levelops.io).', 'default': 'https://api.levelops.io'}},
   {'args':['-p', '--product'], 'kwords':{'dest': 'product', 'help': '(Required if submit is enabled) The id of the corresponding product for the execution of this script.'}}
 ]
